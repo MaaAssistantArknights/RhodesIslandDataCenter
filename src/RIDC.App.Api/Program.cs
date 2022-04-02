0049227c46abd346c1415b5ef472ee7ac411bcf5
Liam Sho <conan_creeper@outlook.com>
2022-04-03 03:39:36 +0800

    init.初始化 API 项目

---
+++ src/RIDC.App.Api/Program.cs
@@ -0,0 +1,81 @@
+using System.Reflection;
+using Serilog;
+
+#region Find Configuration File
+
+var configurationFileEnvironmentVariable = Environment.GetEnvironmentVariable("RIDC_CONFIGURATION_FILE");
+var assemblyPath = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory!.FullName;
+
+string configFile;
+if (File.Exists(configurationFileEnvironmentVariable))
+{
+    configFile = configurationFileEnvironmentVariable;
+}
+else
+{
+    var assemblyPathConfigurationFile = Path.Combine(assemblyPath, "appsettings.json");
+    if (File.Exists(assemblyPathConfigurationFile))
+    {
+        configFile = assemblyPathConfigurationFile;
+    }
+    else
+    {
+        Console.Error.WriteLine("找不到配置文件");
+        return;
+    }
+}
+
+#endregion
+
+#region Configuraion And Logger
+
+var configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile(configFile);
+
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+{
+    configurationBuilder.AddJsonFile(Path.Combine(assemblyPath, "appsettings.Development.json"), true);
+}
+
+configurationBuilder
+    .AddEnvironmentVariables("RIDC:")
+    .AddCommandLine(args);
+
+var configuration = configurationBuilder.Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+Log.Logger.Information("启动中...");
+
+#endregion
+
+#region Web Application Builder
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policyBuilder =>
+    {
+        policyBuilder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+    });
+});
+
+#endregion
+
+#region Pipeline
+
+var app = builder.Build();
+
+app.UseCors();
+app.MapControllers();
+app.Run();
+
+#endregion
