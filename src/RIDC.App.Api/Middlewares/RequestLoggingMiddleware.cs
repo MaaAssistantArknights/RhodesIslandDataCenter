7af00480785d796c6a3fbc3b9ab3ce5b3f6c5f91
Liam Sho <conan_creeper@outlook.com>
2022-04-04 19:02:58 +0800

    perf.优化 Request Logging

--- src/RIDC.App.Api/Middlewares/RequestLoggingMiddleware.cs
+++ src/RIDC.App.Api/Middlewares/RequestLoggingMiddleware.cs
@@ -14,14 +14,65 @@ public class RequestLoggingMiddleware
         _logger = logger;
     }
 
+    private const string Template = "{Method} {Path} {QueryString} {Protocol} with {Body} - {UserAgent} from {Ip} responded {StatusCode} in {Time} ms";
+
+    private record RequestLog(string Method, string Ip, string Path, string QueryString, string Body, string Protocol, string UserAgent);
+    private record ResponseLog(int StatusCode, string Time);
+
     public async Task Invoke(HttpContext context)
     {
+
         var request = context.Request;
-        var path = request.Path;
-        var query = request.QueryString;
+        var requestLog = await FormatRequest(request);
+        var (method, ip, path, queryString, body, protocol, userAgent) = requestLog;
+
+        if (requestLog.UserAgent is "UNKNOWN-USER-AGENT")
+        {
+            await context.Response.WriteAsync("User agent is not supported");
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            _logger.LogWarning(Template, method, path, queryString, protocol, body, userAgent, ip,
+                StatusCodes.Status400BadRequest, "0.00000");
+            return;
+        }
+
+        var sw = new Stopwatch();
+        sw.Start();
+
+        await _next(context);
+
+        sw.Stop();
+
+        var response = context.Response;
+        var responseLog = FormatResponse(response, sw.ElapsedTicks);
+        var (statusCode, time) = responseLog;
+
+        switch (responseLog.StatusCode / 100)
+        {
+            case 4:
+                _logger.LogWarning(Template, method, path, queryString, protocol, body, userAgent, ip, statusCode, time);
+                return;
+            case 5:
+                _logger.LogError(Template, method, path, queryString, protocol, body, userAgent, ip, statusCode, time);
+                return;
+            default:
+                _logger.LogInformation(Template, method, path, queryString, protocol, body, userAgent, ip, statusCode, time);
+                return;
+        }
+    }
+
+    private static async Task<RequestLog> FormatRequest(HttpRequest request)
+    {
+        var path = request.Path.ToString();
+        var query = request.QueryString.ToString();
         var method = request.Method;
-        var scheme = request.Scheme.ToUpper();
-        var remoteIp = request.HttpContext.Connection.RemoteIpAddress?.ToString();
+        var protocol = request.Protocol.ToUpper();
+        var userAgent = request.Headers["User-Agent"].ToString();
+        var remoteIp = request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "UNKNOWN-IP";
+
+        if (string.IsNullOrEmpty(userAgent))
+        {
+            userAgent = "UNKNOWN-USER-AGENT";
+        }
 
         request.EnableBuffering();
         var buffer = new byte[Convert.ToInt32(request.ContentLength)];
@@ -32,30 +83,12 @@ public class RequestLoggingMiddleware
             .Replace(" ", "");
         request.Body.Seek(0, SeekOrigin.Begin);
 
-        var sw = new Stopwatch();
-        sw.Start();
-        await _next(context);
-        sw.Stop();
+        return new RequestLog(method, remoteIp, path, query, bodyAsText, protocol, userAgent);
+    }
 
-        var response = context.Response;
-        var statusCode = response.StatusCode;
-
-        var time = ((float)sw.ElapsedTicks / (TimeSpan.TicksPerMillisecond * 1000)).ToString("F5");
-
-        switch (context.Response.StatusCode)
-        {
-            case StatusCodes.Status200OK:
-                _logger.LogDebug("{Code} {Schema} {Method} {Path} {Query} with {Body} from {Ip} responded in {Time} ms",
-                    statusCode, scheme, method, path, query, bodyAsText, remoteIp, time);
-                break;
-            case StatusCodes.Status404NotFound or StatusCodes.Status500InternalServerError or StatusCodes.Status400BadRequest:
-                _logger.LogError("{Code} {Schema} {Method} {Path} {Query} with {Body} from {Ip}  responded in {Time} ms",
-                    statusCode, scheme, method, path, query, bodyAsText, remoteIp, time);
-                break;
-            default:
-                _logger.LogWarning("{Code} {Schema} {Method} {Path} {Query} with {Body} from {Ip}  responded in {Time} ms",
-                    statusCode, scheme, method, path, query, bodyAsText, remoteIp, time);
-                break;
-        }
+    private static ResponseLog FormatResponse(HttpResponse response, long elapsedTicks)
+    {
+        return new ResponseLog(response.StatusCode,
+            ((float)elapsedTicks / (TimeSpan.TicksPerMillisecond * 1000)).ToString("F5"));
     }
 }
